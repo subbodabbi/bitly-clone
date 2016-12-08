22129057e09a5e78a82bda578b29f47d99961e3a
Amier Ariffin <muhdamierrosli@hotmail.com>
2016-12-08 14:29:47 +0800

    version with no css

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,25 @@
+
 get '/' do
+  @urls = Url.all
+  @title = "Hi"
   erb :"static/index"
+end
+
+
+post '/urls' do
+  @url = Url.new( params["url"])
+  if @url.save
+  	redirect to ('/')
+  else
+    @urls = Url.last(2)
+    @title = "Form"
+    erb :"static/index"
+  end
+end
+
+get "/:short_url" do
+  @url = Url.find_by_short_url(params[:short_url])
+  @url.click_count += 1
+  @url.save
+  redirect to "http://#{@url.long_url}"
 end
---
+++ app/models/url.rb
@@ -0,0 +1,20 @@
+class Url < ActiveRecord::Base
+
+after_initialize :set_click_count
+before_validation :shorten
+validates :long_url, :short_url, presence: true
+validates :short_url, uniqueness: true
+
+  def set_click_count
+    self.click_count ||= 0
+  end
+
+   def shorten
+     self.short_url = SecureRandom.hex(4) 
+   end
+
+  def increment_count
+    self.click_count +=1
+  end
+
+end
